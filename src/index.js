c601c01541f16bcd04dc538f943e79f50f6eccdb
Kristian Behrendt <kristianbg20@mail.com>
2018-10-05 14:55:00 +0200

    added createperson funtion

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ getPersons(populatePersonsTable);
 
 function getPersons(callback) {
 
-    const URL = "http://localhost:8080/CA2/api/person/complete"
+    const URL = "http://localhost:8084/CA2/api/person/complete"
 
     fetch(URL)
         .then(res => res.json())
@@ -31,21 +31,100 @@ function getPersons(callback) {
 function populatePersonsTable(data) {
     let tbody = document.getElementById("table1");
     tbody.innerHTML = "";
+    let columns = new Set(Object.keys(data[0]));
+    columns.delete('id');
     data.forEach(row => {
         let tr = document.createElement("tr");
-        Object.keys(row).forEach(key => {
+        columns.forEach(key => {
             let td = document.createElement("td");
             let value = row[key];
-            if(value instanceof Object){
+            if (value instanceof Object) {
                 td.innerText = value.map(phoneRow => phoneRow['number']).join(", ");
-            } else{
-                td.innerText= value;
+            } else {
+                td.innerText = value != undefined ? value : '';
             }
             tr.appendChild(td);
-            
+
         });
         tbody.appendChild(tr);
     });
+}
+
+/*<form id="create-person-form">
+First name: <input type="text" name="firstName"><br>
+Last name: <input type="text" name="lastName"><br>
+Phone: <input type="text" name="phone"><br>
+Street: <input type="text" name="street"><br>
+AdditionalStreetInfo: <input type="number" name="additionalStreetInfo"><br>
+Zipcode: <input type="text" name="zipcode"><br>
+<input id="submit-person" type="submit" value="Create Person">
+</form>*/
+
+document.getElementById("submit-person").addEventListener("click", function () {
+    let personInfo = document.getElementById("create-person-form");
+    let firstName = personInfo.getAttribute("firstName");
+    let lastName = personInfo.getAttribute("lastName");
+    let phone = personInfo.getAttribute("phone");
+    let street = personInfo.getAttribute("street");
+    let additionalStreetInfo = personInfo.getAttribute("additionalStreetInfo");
+    let zipcode = personInfo.getAttribute("zipcode");
+    function Person(firstName, lastName, phone, street, additionalStreetInfo, zipcode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.street = street;
+        this.additionalStreetInfo = additionalStreetInfo;
+        this.zipcode = zipcode;
+    }
+    let person = new Person(firstName, lastName, phone, street, additionalStreetInfo, zipcode);
+    createPerson(person);
+
+});
+function createPerson(person) {
+
+    const URL = "http://localhost:8084/CA2/api/person"
+
+
+
+
+    fetch(URL, makeOptions("POST", person))
+
+        .then(res => handleHttpErrors(res))
+        .then(data => console.log(data))
+        .catch(err => {
+            if (err.httpError) {
+                err.fullError.then(eJson => console.log(eJson))
+            } else {
+                console.log("Netværksfejl")
+            }
+        })
+
+
+
+    function handleHttpErrors(res) {
+        if (res.ok) {
+            return res.json();
+        } else {
+            return Promise.reject({ httpError: res.status, fullError: res.json() })
+        }
+    }
+
+    function makeOptions(method, body) {
+        var opt = {
+            method: method,
+            headers: {
+                "Content-type": "application/json"
+            }
+        }
+        if (body) {
+            opt.body = JSON.stringify(body);
+        }
+        return opt;
+    }
+
+
+
+
 }
 
 
